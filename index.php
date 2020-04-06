66df3a7ec91cbf028c921bc19c39c7d46f09c853
umihico <umihico@users.noreply.github.com>
2020-04-06 14:25:10 +0900

    serve json

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 require __DIR__ . '/vendor/autoload.php';
 
 lambda(function ($event) {
-    return [
-        'headers' => [
-            'Content-Type' => 'text/html'
-        ],
-        'statusCode' => 200,
-        'body' => '<html><!doctype html><head></head><body><h1>Go Serverless v1.0! Your function executed successfully!</h1></html>',
-    ];
+   return [
+       'headers' => [
+           'Content-Type' => "application/json",
+       ],
+       'statusCode' => 200,
+       'body' => json_encode(['json_key' => 'json_value']),
+   ];
 });
